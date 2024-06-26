2720bcc289a26d37c637535d3e94aaa6aed360b0
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-06-26 20:25:00 +0200

    primo punto extra svolto

--- javascript.js
+++ javascript.js
@@ -10,7 +10,7 @@ fetch("https://striveschool-api.herokuapp.com/books")
   .then((dati) => {
     console.log(dati);
     const rowElement = document.getElementsByClassName("row")[0];
-
+    const listaLibriComprati = [];
     dati.forEach((date) => {
       const colElement = document.createElement("div");
       colElement.classList.add("col-4");
@@ -33,21 +33,59 @@ fetch("https://striveschool-api.herokuapp.com/books")
       pElement.innerText = `${date.price}`;
       pElement.classList.add("card-text");
 
-      const buttonElement = document.createElement("button");
-      buttonElement.classList.add("btn", "btn-primary");
-      buttonElement.innerText = `Scarta`;
+      const buttonElementRemove = document.createElement("button");
+      buttonElementRemove.classList.add("btn", "btn-danger");
+      buttonElementRemove.innerText = `Scarta`;
+
+      const buttonElementAdd = document.createElement("button");
+      buttonElementAdd.classList.add("btn", "btn-primary");
+      buttonElementAdd.innerText = `Compra ora`;
+
+      const buttonRemoveFromChart = document.createElement("button");
+      buttonRemoveFromChart.classList.add("btn", "btn-primary");
+      buttonRemoveFromChart.innerText = `Rimuovi dal carrello`;
 
       divCard.appendChild(imgCard);
       colElement.appendChild(divCard);
       rowElement.appendChild(colElement);
       divBody.appendChild(h5Element);
       divBody.appendChild(pElement);
-      divBody.appendChild(buttonElement);
+      divBody.appendChild(buttonElementRemove);
+      divBody.appendChild(buttonElementAdd);
+      divBody.appendChild(buttonRemoveFromChart);
       divCard.appendChild(divBody);
 
-      buttonElement.addEventListener("click", (event) => {
+      buttonElementRemove.addEventListener("click", (event) => {
         event.target.closest(".col-4").remove();
       });
+
+      buttonElementAdd.addEventListener("click", (event) => {
+        const ulElement = document.getElementsByTagName("ul")[0];
+        console.log(ulElement);
+        const liElement = document.createElement("li");
+        liElement.innerText = date.title;
+        ulElement.appendChild(liElement);
+        listaLibriComprati.push(date.title);
+        // console.log(listaLibriComprati);
+
+        const objToString = JSON.stringify(listaLibriComprati);
+        localStorage.setItem("listaLibriComprati", objToString);
+      });
+
+      //   buttonRemoveFromChart.addEventListener("click", (event) => {
+      //     const listStorage = localStorage.getItem("listaLibriComprati");
+      //     // console.log(listStorage);
+      //     const StringToObj = JSON.parse(listStorage);
+      //     console.log(StringToObj);
+      //     for (let i = 0; i < StringToObj.length; i++) {
+      //       if (StringToObj[i] == date.title) {
+      //         StringToObj.splice(i, 1);
+      //         listaLibriComprati[i] = StringToObj[i];
+      //       }
+      //     }
+      //     console.log(StringToObj);
+      //     // const objToString = JSON.stringify(listaLibriComprati);
+      //   });
     });
   })
 
